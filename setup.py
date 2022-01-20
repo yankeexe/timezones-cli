ad5338a2e2575bb7480f55cc61c52aad359b36b9
Yankee Maharjan <yankee.exe@gmail.com>
2022-01-20 16:40:06 +0545

    Build image for arm64

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requirements_dev = [
 
 setuptools.setup(
     name="timezones_cli",
-    version="0.2.9",
+    version="0.2.10",
     author="Yankee Maharjan",
     url="https://github.com/yankeexe/timezones-cli",
     description="Get local datetime from multiple timezones!",
